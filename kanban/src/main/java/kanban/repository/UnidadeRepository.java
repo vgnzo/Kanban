2e78c4c49ad119dc0af50dc40187afc85fd8730f
Vinicius Galdino <vinicius.galdino@matrizscs.tp>
2026-06-29 08:47:12 -0300

    chore: remove pasta kanban antiga duplicada

--- kanban/src/main/java/kanban/repository/UnidadeRepository.java
+++
@@ -1,8 +0,0 @@
-package kanban.repository;
-
-import kanban.entity.Unidade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface UnidadeRepository extends JpaRepository<Unidade, UUID> {
-}
