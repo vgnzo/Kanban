2b9f3acf52f51153c63e6496a517b6d190a2d7e6
Vinicius Galdino <vinicius.galdino@matrizscs.tp>
2026-07-08 09:55:56 -0300

    feat: Correção de horario do historico

--- backend/src/main/java/kanban/KanbanApplication.java
+++ backend/src/main/java/kanban/KanbanApplication.java
@@ -1,10 +1,19 @@
 package kanban;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class KanbanApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KanbanApplication.class, args);
